2bd967ce38c2a6d91d93c62bdbab178a4cba3c81
Thota <jthota@mcw.edu>
2021-09-08 11:27:07 -0500

    updated

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -1,6 +1,6 @@
 . /etc/profile
 APPNAME=scge-indexing-pipeline
-APPDIR=/home/rgddata/data/pipelines/$APPNAME
+APPDIR=/data/pipelines/$APPNAME
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 EMAIL_LIST=jthota@mcw.edu
 if [ "$SERVER" = "MORN" ]; then
@@ -8,7 +8,7 @@ if [ "$SERVER" = "MORN" ]; then
 fi
 cd $APPDIR
 pwd
-DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
+DB_OPTS="-Dspring.config=/data/pipelines/properties/default_db.xml"
 LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
 export  SCGE_INDEXING_PIPELINE_OPTS="$DB_OPTS $LOG4J_OPTS"
 bin/$APPNAME "$@" | tee run.log
